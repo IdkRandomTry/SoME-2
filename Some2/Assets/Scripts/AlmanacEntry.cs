a5eacfab90014a71f02c84a2b12f6b959c9c514e
PixelRifts <njk777400@gmail.com>
2022-07-28 10:04:11 +0530

    Almanac Entry Animations

--- Some2/Assets/Scripts/AlmanacEntry.cs
+++ Some2/Assets/Scripts/AlmanacEntry.cs
@@ -1,11 +1,10 @@
 using UnityEngine;
 
 public class AlmanacEntry : MonoBehaviour {
-    public UnityEngine.UI.Image content;
+    public AlmanacSelections content;
     public Sprite TheNewContentImage;
 
     public void OnClick() {
-        content.sprite = TheNewContentImage;
-        content.color = new Color(1, 1, 1);
+        content.Switch(TheNewContentImage);
     }
 }
--- Some2/Assets/Scripts/AlmanacSelections.cs
+++ Some2/Assets/Scripts/AlmanacSelections.cs
@@ -1,9 +1,38 @@
 using UnityEngine;
 
 public class AlmanacSelections : MonoBehaviour {
-    private AlmanacEntry[] contents;
+    public Animator ImageOneAnimator;
+    public Animator ImageTwoAnimator;
+    public UnityEngine.UI.Image ImageOne;
+    public UnityEngine.UI.Image ImageTwo;
+
+    private Sprite last_sprite;
+
+    // True if the currently presenting image is ImageTwo
+    // False if the currently presenting image is ImageOne
+    private bool currently_presenting;
 
     void Start() {
-        contents = GetComponentsInChildren<AlmanacEntry>();
+        ImageOneAnimator.SetTrigger("In");
+        ImageTwoAnimator.SetTrigger("Out");
+    }
+
+    public void Switch(Sprite sprite) {
+        if (currently_presenting) {
+            ImageOne.sprite = sprite;
+            ImageOne.color = new Color(1, 1, 1);
+            ImageTwo.sprite = last_sprite;
+            if (last_sprite != null) ImageTwo.color = new Color(1, 1, 1);
+            ImageTwoAnimator.SetTrigger("Out");
+            ImageOneAnimator.SetTrigger("In");
+        } else {
+            ImageTwo.sprite = sprite;
+            ImageTwo.color = new Color(1, 1, 1);
+            ImageOne.sprite = last_sprite;
+            if (last_sprite != null) ImageOne.color = new Color(1, 1, 1);
+            ImageOneAnimator.SetTrigger("Out");
+            ImageTwoAnimator.SetTrigger("In");
+        }
+        last_sprite = sprite;
     }
 }
