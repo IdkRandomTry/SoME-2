31aeb532e3441cf5db32cb7e0a5d70509f1b9800
SiddBourbaki <umarjeesiddhesh@gmail.com>
2022-08-12 22:56:53 +0530

    hints halfway done

--- Some2/Assets/Scripts/HintButton.cs
+++ Some2/Assets/Scripts/HintButton.cs
@@ -4,6 +4,8 @@ public class HintButton : MonoBehaviour {
     public string HintText;
 
     private GameObject m_hint_object;
+    private GameObject m_hint_arrows;
+    private GameObject m_reclick_arrow;
     private TMPro.TMP_Text m_text;
 
     private bool had_clicked;
@@ -11,6 +13,8 @@ public class HintButton : MonoBehaviour {
 
     void Start() {
         m_hint_object = transform.Find("Hint").gameObject;
+        m_hint_arrows =  transform.Find("Hint").Find("HintArrows").gameObject;
+        m_reclick_arrow =  transform.Find("Hint").Find("ArrowBob").gameObject;
         m_text = transform.Find("Hint").Find("HintText").GetComponent<TMPro.TMP_Text>();
         m_hint_object.SetActive(false);
         had_clicked = false;
@@ -22,15 +26,19 @@ public class HintButton : MonoBehaviour {
             if (!had_clicked) {
                 m_hint_object.SetActive(true);
                 had_clicked = true;
-                m_text.text = "Are you sure you want a hint?";
+                m_text.text = "Are you sure you want a hint? \nClick again to reveal hint";
+                m_hint_arrows.SetActive(false);
             } else {
                 m_hint_object.SetActive(true);
                 m_text.text = HintText;
+                m_hint_arrows.SetActive(true);
+                m_reclick_arrow.SetActive(false);
             }
             m_hint_object.SetActive(true);
             is_active = true;
         } else {
             m_hint_object.SetActive(false);
+            m_hint_arrows.SetActive(false);
             is_active = false;
         }
     }
